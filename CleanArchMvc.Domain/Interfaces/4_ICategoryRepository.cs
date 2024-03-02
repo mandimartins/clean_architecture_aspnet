0e32139c9fa3b9d4c00ffec26dfec4e17bf096de
Amanda Martins <mandimartins23@gmail.com>
2024-03-02 17:44:28 -0300

    feat: add Interfaces

---
+++ CleanArchMvc.Domain/Interfaces/4_ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCatories();
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
