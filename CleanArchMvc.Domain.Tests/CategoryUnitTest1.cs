ede8d1b3507adbc53120e93af5169c3ce95cacbc
Amanda Martins <mandimartins23@gmail.com>
2024-03-02 18:41:26 -0300

    feat: add Domain tests for Category

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,51 @@
+using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name ");
+            action.Should()
+                 .NotThrow<CleanArchMvc.Domain.Validations.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name ");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validations.DomainExceptionValidation>()
+                 .WithMessage("Invalid Id value.");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validations.DomainExceptionValidation>()
+                   .WithMessage("Invalid Name. Too short, minimum 3 characters.");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validations.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required.");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validations.DomainExceptionValidation>();
+        }
+    }
+}
