d6090cec9fa9663da3d7a32428a3f266cec16303
Amanda Martins <mandimartins23@gmail.com>
2024-03-03 13:09:11 -0300

    feat: add DbContext Class

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options){}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
