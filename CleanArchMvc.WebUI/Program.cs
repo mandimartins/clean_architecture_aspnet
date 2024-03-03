bf78d00375a31f2e18c04e7a1cfeae62cefecb0b
Amanda Martins <mandimartins23@gmail.com>
2024-03-03 18:41:54 -0300

    feat: add DependencyInjection file and configuration to the WebUI layer

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,6 +1,9 @@
+using CleanArchMvc.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
