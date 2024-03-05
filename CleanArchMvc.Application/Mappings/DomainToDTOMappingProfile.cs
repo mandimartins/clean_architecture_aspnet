fcfa621920d8e8a5eb9baec33fd9dd80daee45da
Amanda Martins <mandimartins23@gmail.com>
2024-03-05 20:29:03 -0300

    feat: Add DTOs and Mappings with AutoMapper

---
+++ CleanArchMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile: Profile 
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();  
+        }
+    }
+}
