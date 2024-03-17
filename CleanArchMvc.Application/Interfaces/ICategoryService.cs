5a3131d4651bdd84e341f6d2fef699f726a2bada
Amanda Martins <mandimartins23@gmail.com>
2024-03-17 12:25:53 -0300

    feat: add Product and Category services

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
+        Task Remove(int? id);
+    }
+}
