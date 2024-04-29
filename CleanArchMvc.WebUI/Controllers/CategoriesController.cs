8edf452c1bf1daf21105a6ed26e2565671f5b987
Amanda Martins <mandimartins23@gmail.com>
2024-04-29 19:11:24 -0300

    feat: add CRUD for Category

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,16 +1,17 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class CategoriesController : Controller
     {
-
         private readonly ICategoryService _categoryService;
-
         public CategoriesController(ICategoryService categoryService)
         {
-            this._categoryService = categoryService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -18,6 +19,83 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var categories = await _categoryService.GetCategories();
             return View(categories);
+        }
+
+        [HttpGet()]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var categoryDto = await _categoryService.GetById(id);
+            if (categoryDto == null) return NotFound();
+            return View(categoryDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(categoryDto);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _categoryService.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null)
+                return NotFound();
+
+            return View(categoryDto);
         }
     }
 }
